e031e966bd0e8452504abf0b2feccff7dcd8a8f4
draiguv <dcrauv13@gmail.com>
2023-08-06 02:56:14 +0800

    Complete the challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,46 @@ export default function App() {
     email: ""
   });
 
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  
+  const updateContact = e => {
+    const {name, value} = e.target;
+
+    setContact(prevVal => {
+      if (name === 'fName') {
+        return {
+          fName: value,
+          lName: prevVal.lName,
+          email: prevVal.email,
+        }
+      } else if (name === 'lName') {
+        return {
+          fName: prevVal.fName,
+          lName: value,
+          email: prevVal.email,
+        }
+      } else {
+        return {
+          fName: prevVal.fName,
+          lName: prevVal.lName,
+          email: value,
+        }
+      }
+    });
+  }
+
   return (
     <div className="container">
       <h1>
         Hello {contact.fName} {contact.lName}
       </h1>
       <p>{contact.email}</p>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+      <form onSubmit={handleSubmit}>
+        <input onChange={updateContact} name="fName" placeholder="First Name" value={contact.fName} />
+        <input onChange={updateContact} name="lName" placeholder="Last Name" value={contact.lName} />
+        <input onChange={updateContact} name="email" placeholder="Email" value={contact.email} />
         <button>Submit</button>
       </form>
     </div>
